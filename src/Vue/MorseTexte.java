ac1e38992a6bd82d7e58f3ee283b8ae1a32067f8
Challier <challieremmanuel@yahoo.fr>
2014-01-22 21:40:15 +0100

    Boucle pour coder en morse plusieurs phrase

--- src/Vue/MorseTexte.java
+++ src/Vue/MorseTexte.java
@@ -8,39 +8,46 @@ public class MorseTexte {
 	//--ATTRIBUT--
 	private Morse morseModel;
 	private Scanner sc;
-	
+
 	//--CONSTRUCTEUR--
 	public MorseTexte(Morse m){
 		morseModel = m;
 		sc = new Scanner(System.in);
 		startMorse();
 	}
-	
+
 	//--METHODE--
 	private void startMorse(){
-		String chaineACoder = "";
-		int choix = 1;
-		System.out.println("Entrez la phrase a coder en morse: ");
-		chaineACoder = sc.nextLine();
-		System.out.println("Choisissez le mode de codage");
-		System.out.println("1: Normal  2: Inversé  3: ApparenceBinaire");
-		try{
-			choix = Integer.parseInt(sc.next());
-			switch (choix) {
-			case 2: morseModel.setInverse(true); break;
-			case 3: morseModel.setBinaire(true); break;
-			default: 
-				morseModel.setInverse(false);
-				morseModel.setBinaire(false);
-				break;
+		while(true){
+			String chaineACoder = "";
+			int choix = 1;
+			System.out.println("Entrez la phrase à coder en morse: ");
+			chaineACoder = sc.nextLine();
+			System.out.println("Choisissez le mode de codage");
+			System.out.println("1: Normal  2: Inversé  3: ApparenceBinaire");
+			try{
+				choix = Integer.parseInt(sc.next());
+				switch (choix) {
+				case 1: morseModel.setInverse(false); morseModel.setBinaire(false); break; 
+				case 12: morseModel.setInverse(true); morseModel.setBinaire(false); break;
+				case 21: morseModel.setInverse(true); morseModel.setBinaire(false); break;
+				case 3: morseModel.setInverse(false); morseModel.setBinaire(true); break;
+				case 23: morseModel.setInverse(true); morseModel.setBinaire(true); break;
+				case 32: morseModel.setInverse(true); morseModel.setBinaire(true); break;
+				default: 
+					morseModel.setInverse(false);
+					morseModel.setBinaire(false);
+					break;
+				}
+			}catch(NumberFormatException e){
+				System.out.println("Valeur invalide");
+				return ;
 			}
-		}catch(NumberFormatException e){
-			System.out.println("Valeur invalide");
-			return ;
+			System.out.println(morseModel.morse(chaineACoder));
+			sc.nextLine();
 		}
-		System.out.println(morseModel.morse(chaineACoder));
 	}
-	
+
 	//--TESTE--
 	public static void main(String[] args){
 		Morse modele = new Morse();
